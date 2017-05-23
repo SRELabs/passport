40c27e488e1fc9b786321c76cdd6a933e31cd1c3
dengyun <dengyun@ttlsa.com>
2017-05-23 09:18:26 +0800

    增加md5验签方法

--- library/api_enc.py
+++ library/api_enc.py
@@ -4,8 +4,8 @@
 sign验证
 """
 from hashlib import md5
+from django.conf import settings
 import types
-from library.apienc.config import Settings
 import time
 
 
@@ -14,8 +14,10 @@ class ApiEnc:
     运维平台客户端
     """
     # 项目信息
-    def __init__(self):
-        pass
+    def __init__(self, key=settings.APP_KEY, charset=settings.APP_INPUT_CHARSET, sign=settings.APP_SIGN_TYPE):
+        self.APP_KEY = key
+        self.APP_INPUT_CHARSET = charset
+        self.APP_SIGN_TYPE = sign
 
     # 字符串编解码处理
     def smart_str(self, s, encoding='utf-8', strings_only=False, errors='strict'):
@@ -46,31 +48,30 @@ class ApiEnc:
         prestr = ''
         for k in ks:
             v = params[k]
-            k = self.smart_str(k, Settings.CS_INPUT_CHARSET)
+            k = self.smart_str(k, self.APP_INPUT_CHARSET)
             if k not in ('sign', 'sign_type') and v != '':
-                newparams[k] = self.smart_str(v, Settings.CS_INPUT_CHARSET)
+                newparams[k] = self.smart_str(v, self.APP_INPUT_CHARSET)
                 prestr += '%s=%s&' % (k, newparams[k])
         prestr = prestr[:-1]
         return newparams, prestr
 
     # 生成签名结果,str
-    @staticmethod
-    def build_mysign_str(prestr, sign_type='MD5'):
+    def build_mysign_str(self, prestr, sign_type='MD5'):
         if sign_type == 'MD5':
-            return md5(prestr + Settings.CS_KEY).hexdigest()
+            return md5(prestr + self.APP_KEY).hexdigest()
         return ''
 
     # 生成签名结果,dict
     def build_mysign(self, data, sign_type='MD5'):
         _, prestr = self.params_filter(data)
         if sign_type == 'MD5':
-            return md5(prestr + Settings.CS_KEY).hexdigest()
+            return md5(prestr + self.APP_KEY).hexdigest()
         return ''
 
     # 初级验证--签名
     def notify_verify(self, post):
         _, prestr = self.params_filter(post, False)
-        mysign = self.build_mysign_str(prestr, Settings.CS_SIGN_TYPE)
+        mysign = self.build_mysign_str(prestr, self.APP_SIGN_TYPE)
         # mysign = self.build_mysign(post)
         if mysign == post.get('sign') and self.expires_verify(post.get('expires', 0)):
             return True
@@ -82,7 +83,6 @@ class ApiEnc:
     def expires_verify(expires):
         if expires:
             curr_time = int(str(time.time())[0:-3])
-            if int(expires) > curr_time:
-                return True
+            return True if int(expires) > curr_time else False
         else:
             return False
